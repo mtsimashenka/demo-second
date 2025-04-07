9836c22c9fbd6e209d7c596c2870e78077e0f215
mTimoshenko <maksim-timoshenko-97@mail.ru>
2025-04-07 15:15:06 +0300

    add getter setter

--- .idea/src/main/java/User.java
+++ .idea/src/main/java/User.java
@@ -1,10 +1,43 @@
-class User{
-    public class User{
+import java.util.Objects;
+
+public class User {
         private String login;
         private String password;
 
-        public User(String login) {
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
             this.login = login;
         }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "login='" + login + '\'' +
+                    ", password='" + password + '\'' +
+                    '}';
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(login, user.login) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, password);
     }
 }
+
